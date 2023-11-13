9634019a553358dfd7b9956ed5d57279890872a2
Kamal Shah <kkamal714@gmail.com>
2023-11-13 12:05:36 +0530

    Add dependencies for email functionality and update HelperService tests

--- src/main/java/com/kamalsha/helper/commonutils/services/HelperService.java
+++ src/main/java/com/kamalsha/helper/commonutils/services/HelperService.java
@@ -7,20 +7,28 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.text.DecimalFormat;
 
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import lombok.extern.log4j.Log4j2;
+// IOException
+import java.io.IOException;
 
 /**
  * A utility service providing various helper methods for common tasks.
  */
 @Service
 @Log4j2
-public class Helper {
+public class HelperService {
 
   private static final String NA = "NA";
   private static final String DATE_FORMAT_INPUT = "dd/MM/yyyy";
-  private static final String DATE_FORMAT_OUTPUT = "yyyy-MM-dd";
 
   /**
    * Converts a string to a double value.
@@ -29,19 +37,43 @@ public class Helper {
    * @return the double value of the string, or 0 if the string cannot be
    *         converted
    */
-  public double getDoubleValue(String str) {
-    double result = 0;
-    try {
-      if (str != null) {
-        String in = str.trim();
-        if (!in.isEmpty()) {
-          result = Double.parseDouble(in);
-        }
-      }
-    } catch (NumberFormatException e) {
-      log.error("Error in getDoubleValue: Unable to convert string to double. Input: " + str, e);
+  public double toDouble(String str) {
+    return toDouble(str, 2); // Default value for decimalPlaces is 2
+  }
+
+  public double toDouble(String str, Integer decimalPlaces) {
+
+    if (str == null || str.trim().isEmpty() || str.equals("NA") || str.equalsIgnoreCase("")) {
+      return 0;
     }
-    return result;
+
+    String decimalPlaceValue = "#.##";
+
+    if (decimalPlaces != null && decimalPlaces > 0) {
+      decimalPlaceValue = "#.";
+      for (int i = 0; i < decimalPlaces; i++) {
+        decimalPlaceValue += "#";
+      }
+    }
+
+    String newValue = str.replaceAll("[^\\d.]", "");
+    if (newValue.isEmpty()) {
+      return 0;
+    }
+    if (newValue.contains(".")) {
+      newValue = newValue.substring(0, newValue.indexOf("."));
+    }
+
+    double number = Double.parseDouble(newValue);
+    DecimalFormat df = new DecimalFormat(decimalPlaceValue);
+    number = Double.parseDouble(df.format(number));
+
+    if (str.contains("-")) {
+      number *= -1;
+    }
+
+    return number;
+
   }
 
   /**
@@ -51,7 +83,7 @@ public class Helper {
    * @return the integer value of the string, or 0 if the string cannot be
    *         converted
    */
-  public Integer getInteger(String value) {
+  public Integer toInt(String value) {
     if (value == null || value.isEmpty() || value.equals("NA") || value.equalsIgnoreCase("")) {
       return 0;
     }
@@ -78,7 +110,7 @@ public class Helper {
    * @param obj the object to be converted
    * @return a HashMap containing the object's fields and their values
    */
-  public Map<String, Object> convertObjectToHashMap(Object obj) {
+  public Map<String, Object> toHashMap(Object obj) {
     HashMap<String, Object> map = new HashMap<>();
     for (java.lang.reflect.Field field : obj.getClass().getDeclaredFields()) {
       field.setAccessible(true);
@@ -97,7 +129,7 @@ public class Helper {
    * @param date the date string to be formatted
    * @return the formatted date string, or the original string if parsing fails
    */
-  public String formatDate(String date) {
+  public String toDate(String date) {
     String formattedDate = "";
     if (isValidDateFormat(date)) {
       try {
@@ -176,6 +208,77 @@ public class Helper {
    */
   public boolean isDate(Object value) {
     return value instanceof Date;
+  }
+
+  // To Capitalize first letter of string
+  public String capitalize(String str) {
+    if (str == null || str.isEmpty()) {
+      return str;
+    }
+
+    return str.substring(0, 1).toUpperCase() + str.substring(1);
+  }
+
+  /**
+   * Parses a JSON string and returns a Map object representing the JSON object.
+   *
+   * @param jsonString the JSON string to be parsed
+   * @return a Map object representing the JSON object
+   */
+  public Map<String, Object> getJsonObject(String jsonString) {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    Map<String, Object> jsonObj = null;
+    try {
+      jsonObj = mapper.readValue(jsonString, Map.class);
+    } catch (JsonMappingException e) {
+      log.error("Error in getJsonObject: " + e.getMessage());
+    } catch (JsonProcessingException e) {
+      log.error("Error in getJsonObject: " + e.getMessage());
+    } catch (IOException e) {
+      log.error("Error in getJsonObject: " + e.getMessage());
+    }
+    return jsonObj;
+  }
+
+  /**
+   * Returns the value of the nested key in the given map.
+   *
+   * @param <T>  the type of the value to be returned
+   * @param map  the map to search for the nested key
+   * @param keys the keys to search for the nested value
+   * @return the value of the nested key in the given map
+   */
+  public static <T> T getNestedValue(Map<String, Object> map, String... keys) {
+    Object value = null;
+
+    if (map == null) {
+      return null;
+    }
+
+    for (String key : keys) {
+      if (map == null) {
+        break;
+      }
+
+      // If key is not found, return null
+      if (!map.containsKey(key)) {
+        value = null;
+        break;
+      }
+
+      value = map.get(key);
+
+      if (value instanceof Map) {
+        // noinspection unchecked
+        map = (Map<String, Object>) value;
+      } else {
+        break; // Stop if the value is not a Map
+      }
+    }
+
+    // noinspection unchecked
+    return (T) value;
   }
 
 }
--- src/test/java/com/kamalsha/helper/commonutils/services/HelperTest.java
+++ src/test/java/com/kamalsha/helper/commonutils/services/HelperTest.java
@@ -2,41 +2,41 @@ package com.kamalsha.helper.commonutils.services;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.*;
 import java.util.Date;
 
 class HelperTest {
 
-  private final Helper helper = new Helper();
+  private final HelperService helper = new HelperService();
 
   @Test
-  void testGetDoubleValue() {
-    assertEquals(5.0, helper.getDoubleValue("5.0"));
-    assertEquals(0.0, helper.getDoubleValue("invalid"));
-    assertEquals(0.0, helper.getDoubleValue(""));
-    assertEquals(0.0, helper.getDoubleValue(null));
-    assertEquals(0.0, helper.getDoubleValue("5.0.0"));
+  void testtoDouble() {
+    assertEquals(5.0, helper.toDouble("5.0"));
+    assertEquals(0.0, helper.toDouble("invalid"));
+    assertEquals(0.0, helper.toDouble(""));
+    assertEquals(0.0, helper.toDouble(null));
+    assertEquals(5.0, helper.toDouble("5.0.0"));
   }
 
   @Test
-  void testGetInteger() {
-    assertEquals(10, helper.getInteger("10"));
-    assertEquals(-5, helper.getInteger("-5"));
-    assertEquals(0, helper.getInteger("invalid"));
-    assertEquals(0, helper.getInteger(""));
-    assertEquals(0, helper.getInteger(null));
-    assertEquals(123456789, helper.getInteger("123,456,789"));
-    assertEquals(-123456789, helper.getInteger("-123,456,789"));
+  void testtoInt() {
+    assertEquals(10, helper.toInt("10"));
+    assertEquals(-5, helper.toInt("-5"));
+    assertEquals(0, helper.toInt("invalid"));
+    assertEquals(0, helper.toInt(""));
+    assertEquals(0, helper.toInt(null));
+    assertEquals(123456789, helper.toInt("123,456,789"));
+    assertEquals(-123456789, helper.toInt("-123,456,789"));
   }
 
   @Test
   void testConvertObjectToHashMap() {
     // Assuming you have a sample object to test with
     SampleObject sampleObject = new SampleObject("field1", 42, true);
-    assertEquals(3, helper.convertObjectToHashMap(sampleObject).size());
-    assertEquals("field1", helper.convertObjectToHashMap(sampleObject).get("field1"));
-    assertEquals(42, helper.convertObjectToHashMap(sampleObject).get("field2"));
-    assertEquals(true, helper.convertObjectToHashMap(sampleObject).get("field3"));
+    assertEquals(3, helper.toHashMap(sampleObject).size());
+    assertEquals("field1", helper.toHashMap(sampleObject).get("field1"));
+    assertEquals(42, helper.toHashMap(sampleObject).get("field2"));
+    assertEquals(true, helper.toHashMap(sampleObject).get("field3"));
   }
 
   private static class SampleObject {
@@ -52,13 +52,13 @@ class HelperTest {
   }
 
   @Test
-  void testFormatDate() {
-    assertEquals("2022-01-15", helper.formatDate("15/01/2022"));
-    assertEquals("", helper.formatDate("invalid"));
-    assertEquals("", helper.formatDate(""));
-    assertEquals("", helper.formatDate(null));
-    assertEquals("", helper.formatDate("15-01-2022"));
-    assertEquals("", helper.formatDate("2022/01/15"));
+  void testtoDate() {
+    assertEquals("2022-01-15", helper.toDate("15/01/2022"));
+    assertEquals("", helper.toDate("invalid"));
+    assertEquals("", helper.toDate(""));
+    assertEquals("", helper.toDate(null));
+    assertEquals("", helper.toDate("15-01-2022"));
+    assertEquals("", helper.toDate("2022/01/15"));
   }
 
   @Test
@@ -81,5 +81,41 @@ class HelperTest {
     assertTrue(helper.isDate(new Date()));
     assertFalse(helper.isDate("2022-01-15"));
     assertFalse(helper.isDate(null));
+  }
+
+  @Test
+  void testGetNestedValue() {
+    Map<String, Object> map = new HashMap<>();
+    map.put("key1", "value1");
+    map.put("key2", new HashMap<String, Object>() {
+      {
+        put("key3", "value3");
+        put("key4", new HashMap<String, Object>() {
+          {
+            put("key5", "value5");
+          }
+        });
+      }
+    });
+
+    assertEquals("value1", helper.getNestedValue(map, "key1"));
+    assertEquals("value3", helper.getNestedValue(map, "key2", "key3"));
+    assertEquals("value5", helper.getNestedValue(map, "key2", "key4", "key5"));
+    assertNull(helper.getNestedValue(map, "key2", "key4", "key6"));
+    assertNull(helper.getNestedValue(map, "key3"));
+    assertNull(helper.getNestedValue(null, "key1"));
+
+  }
+
+  @Test
+  void testGetJsonObject() {
+
+    String jsonString = "{\"name\":\"John\", \"age\":30, \"car\":null}";
+    Map<String, Object> expected = new HashMap<>();
+    expected.put("name", "John");
+    expected.put("age", 30);
+    expected.put("car", null);
+    Map<String, Object> actual = helper.getJsonObject(jsonString);
+    assertEquals(expected, actual);
   }
 }
